bad31b7e531450e7c8c8226545c94657e6a30154
JoseVicente19 <josechepe717@gmail.com>
2025-11-05 16:02:05 -0600

    actu STATICFILES_DIRS

--- PFDESARROLLO/settings.py
+++ PFDESARROLLO/settings.py
@@ -158,9 +158,7 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static', 
-]
+STATICFILES_DIRS = []
 
 STORAGES = {
     # ...
@@ -169,6 +167,7 @@ STORAGES = {
     },
 }
 
+WHITENOISE_MANIFEST_STRICT = False
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
