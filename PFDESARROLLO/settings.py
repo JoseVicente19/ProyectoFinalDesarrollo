b3beb223b733b8a6aedfe31d9f4f562ffcd96e88
JoseVicente19 <josechepe717@gmail.com>
2025-11-04 23:08:04 -0600

    actu config deploy

--- PFDESARROLLO/settings.py
+++ PFDESARROLLO/settings.py
@@ -102,17 +102,17 @@ WSGI_APPLICATION = 'PFDESARROLLO.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': os.environ.get('MYSQLDATABASE', ''), 
-        'USER': os.environ.get('MYSQLUSER', ''), 
-        'PASSWORD': os.environ.get('MYSQLPASSWORD', ''), 
-        'HOST': os.environ.get('MYSQLHOST', ''), 
-        'PORT': os.environ.get('MYSQLPORT', None), 
-        'OPTIONS': {
-            'unix_socket': None,
-        }
-    }
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': os.environ.get('MYSQLDATABASE', ''), 
+        'USER': os.environ.get('MYSQLUSER', ''), 
+        'PASSWORD': os.environ.get('MYSQLPASSWORD', ''), 
+        'HOST': os.environ.get('MYSQLHOST', ''), 
+        'PORT': os.environ.get('MYSQLPORT', None), 
+        'OPTIONS': {
+            'unix_socket': None,
+        }
+    }
 }
 
 # Password validation
